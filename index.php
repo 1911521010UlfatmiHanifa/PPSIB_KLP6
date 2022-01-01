a50bdf28e5ebd15efa7ed693d1feca73d60dfd69
Ulfatmi Hanifa <1911521010_ulfatmi@student.unand.ac.id>
2022-01-02 06:50:18 +0700

    first commit3

---
+++ index.php
@@ -0,0 +1,246 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Home</title>
+  <meta content="" name="descriptison">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+	<link href="assets2/img/logo.png" rel="icon">
+	<link href="assets2/img/logo.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link
+    href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+    rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets2/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets2/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="assets2/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets2/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="assets2/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="assets2/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets2/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Appland - v2.2.0
+  * Template URL: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top  header-transparent ">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo mr-auto">
+        <h1 class="text-light"><img src="assets2/img/logo.png" alt="" class="img-fluid">  Unit Kegiatan Seni Universitas Andalas</a></h1>
+      </div>
+
+      <nav class="nav-menu d-none d-lg-block">
+        <ul>
+          <li class="active"><a href="index.php">Home</a></li>
+          <li><a href="login.php">Login</a></li>
+        </ul>
+      </nav><!-- .nav-menu -->
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= Hero Section ======= -->
+  <section id="hero" class="d-flex align-items-center">
+
+    <div class="container">
+      <div class="row">
+        <div
+          class="col-lg-6 d-lg-flex flex-lg-column justify-content-center align-items-stretch pt-5 pt-lg-0 order-2 order-lg-1"
+          data-aos="fade-up">
+          <div>
+            <h1>Pendaftaran Akun Peminjam Baru</h1>
+            <a href="register.php" class="download-btn">Klik Disini!</a>
+          </div>
+        </div>
+        <div class="col-lg-6 d-lg-flex flex-lg-column align-items-stretch order-1 order-lg-2 hero-img"
+          data-aos="fade-up">
+          <img src="assets2/img/logo.png" class="img-fluid" alt="">
+        </div>
+      </div>
+    </div>
+
+  </section><!-- End Hero -->
+
+  <main id="main">
+    <section id="gallery" class="gallery">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Gallery</h2>
+          <p>Dokumentasi Kegiatan Unit Kegiatan Mahasiswa Universitas Andalas</p>
+        </div>
+
+        <div class="owl-carousel gallery-carousel" data-aos="fade-up">
+          <a href="assets2/img/gallery/gambar1.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar1.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar2.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar2.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar3.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar3.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar4.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar4.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar5.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar5.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar6.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar6.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar7.png" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar7.png" alt=""></a>
+          <a href="assets2/img/gallery/gambar8.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar8.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar9.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar9.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar10.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar10.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar11.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar11.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar12.jpeg" class="venobox" data-gall="gallery-carousel"><img
+              src="assets2/img/gallery/gambar12.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar13.jpeg" class="venobox" data-gall="gallery-carousel"><img
+            src="assets2/img/gallery/gambar13.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar14.jpeg" class="venobox" data-gall="gallery-carousel"><img
+            src="assets2/img/gallery/gambar14.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar15.jpeg" class="venobox" data-gall="gallery-carousel"><img
+            src="assets2/img/gallery/gambar15.jpeg" alt=""></a>
+          <a href="assets2/img/gallery/gambar16.jpeg" class="venobox" data-gall="gallery-carousel"><img
+            src="assets2/img/gallery/gambar16.jpeg" alt=""></a>
+        </div>
+
+      </div>
+    </section><!-- End Gallery Section -->
+
+    <!-- ======= Contact Section ======= -->
+    <section id="contact" class="contact">
+      <div class="container">
+
+        <div class="section-title">
+          <h2>Unit Kegiatan Mahasiswa Universitas Andalas</h2>
+          <p>Unit Kegiatan Seni (UKS) merupakan salah satu organisasi mahasiswa intra kampus yang berkaitan dengan bidang seni di 
+            tingkat Universitas. UKS berdiri pada tahun 1990 yang dipelopori oleh kelompok paduan suara dengan nama UKM Kesenian.
+            UKS Unand memiliki 6 divisi yaitu divisi administrasi, divisi keuangan, divisi humas, divisi SDM, divisi pemasaran 
+            dan divisi rumah tangga</p>
+        </div>
+
+        <div class="row">
+
+          <div class="col-lg-6">
+            <div class="col-lg-14 d-lg-flex flex-lg-column align-items-stretch order-5 order-lg-4 hero-img"
+              data-aos="fade-up">
+              <img src="assets2/img/logo.jpeg" class="img-fluid" alt="">
+            </div>
+          </div>
+
+          <div class="col-lg-6">
+            <div class="row">
+              <div class="col-lg-6 info" data-aos="fade-up">
+                <i class="bx bx-map"></i>
+                <h4>Address</h4>
+                <p>Jl. Limau Manis, Gedung PKM Lantai 2, Universitas Andalas</p>
+              </div>
+              <div class="col-lg-6 info" data-aos="fade-up" data-aos-delay="100">
+                <i class="bx bx-phone"></i>
+                <h4>Call Us</h4>
+                <p><br>0812-7067-4250</p>
+              </div>
+              <div class="col-lg-6 info" data-aos="fade-up" data-aos-delay="200">
+                <i class="bx bx-envelope"></i>
+                <h4>Email Us</h4>
+                <p><br>uksunand@gmail.com</p>
+              </div>
+              <div class="col-lg-6 info" data-aos="fade-up" data-aos-delay="300">
+                <i class="bx bx-time-five"></i>
+                <h4>Working Hours</h4>
+                <p>Open<br>08.00 - 16.00</p>
+              </div>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+    </section><!-- End Contact Section -->
+
+  </main><!-- End #main -->
+
+  <!-- ======= Footer ======= -->
+  <footer id="footer">
+
+    <div class="footer-top">
+      <div class="container">
+        <div class="row">
+
+          <div class="col-lg-3 col-md-6 footer-contact" data-aos="fade-up">
+            <h3>Unit Kegiatan Mahasiswa Universitas Andalas</h3>
+            <p>
+              Jl. Limau Manis, Gedung PKM Lantai 2, Universitas Andalas, <br>
+              Padang, Sumatra Barat <br><br>
+              <strong>Phone:</strong> 0812-7067-4250<br>
+              <strong>Email:</strong> uksunand@gmail.com<br>
+            </p>
+          </div>
+
+          <div class="col-lg-3 col-md-6 footer-links" data-aos="fade-up" data-aos-delay="300">
+            <h4>Our Social Networks</h4>
+            <p></p>
+            <div class="social-links mt-3">
+              <!-- <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a> -->
+              <a href="https://www.facebook.com/uks_unand" class="facebook"><i class="bx bxl-facebook"></i></a>
+              <a href="https://www.instagram.com/uks_unand/" class="instagram"><i class="bx bxl-instagram"></i></a>
+              <a href="https://www.youtube.com/channel/UCldLMGLN2ObnqSiwBrbDOtQ" class="youtube"><i class="bx bxl-youtube"></i></a>
+              <!-- <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
+              <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a> -->
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+
+    <div class="container py-4">
+      <div class="copyright">
+        &copy; Copyright <strong><span>Appland</span></strong>. All Rights Reserved
+      </div>
+      <div class="credits">
+        <!-- All the links in the footer should remain intact. -->
+        <!-- You can delete the links only if you purchased the pro version. -->
+        <!-- Licensing information: https://bootstrapmade.com/license/ -->
+        <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/free-bootstrap-app-landing-page-template/ -->
+        Program by Kelompok 6 PPSI</a>
+      </div>
+    </div>
+  </footer><!-- End Footer -->
+
+  <a href="#" class="back-to-top"><i class="icofont-simple-up"></i></a>
+
+  <!-- Vendor JS Files -->
+  <script src="assets2/vendor/jquery/jquery.min.js"></script>
+  <script src="assets2/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets2/vendor/jquery.easing/jquery.easing.min.js"></script>
+  <script src="assets2/vendor/php-email-form/validate.js"></script>
+  <script src="assets2/vendor/owl.carousel/owl.carousel.min.js"></script>
+  <script src="assets2/vendor/venobox/venobox.min.js"></script>
+  <script src="assets2/vendor/aos/aos.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets2/js/main.js"></script>
+
+</body>
+
+</html>
